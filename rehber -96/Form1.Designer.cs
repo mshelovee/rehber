18fc0a0f3b59b1f02aa8bafd243f93f670904bfb
hilal <130781977+mshelovee@users.noreply.github.com>
2023-11-20 11:33:04 +0300

    Add files via upload

---
+++ rehber -96/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace rehber__96
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbAdlar = new System.Windows.Forms.ListBox();
+            this.btnAra = new System.Windows.Forms.Button();
+            this.txtAranan = new System.Windows.Forms.TextBox();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbAdlar
+            // 
+            this.lbAdlar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbAdlar.ForeColor = System.Drawing.Color.Black;
+            this.lbAdlar.FormattingEnabled = true;
+            this.lbAdlar.ItemHeight = 16;
+            this.lbAdlar.Items.AddRange(new object[] {
+            "Raito",
+            "Yezna(çakma messi)",
+            "Ravza Sanchez",
+            "Mshel(gerçek messi)",
+            "Miurady",
+            "Miesa",
+            "Mocha(balim)",
+            "Raito",
+            "Mocha(balim)",
+            "Miurady",
+            "Miesa",
+            "Rowena",
+            "Mshel(gerçek messi)",
+            "Yezna(çakma messi)",
+            "Mocha(balim)",
+            "Durmuş(durmamış)",
+            "Rowena",
+            "EdaBalım",
+            "Müco",
+            "Rüzgar",
+            "Müco",
+            "Bulut(kedim)",
+            "Oscar(uzaylı kedi)",
+            "Rüzgar",
+            "EdaBalım",
+            "Aney",
+            "Cino",
+            ""});
+            this.lbAdlar.Location = new System.Drawing.Point(12, 84);
+            this.lbAdlar.Name = "lbAdlar";
+            this.lbAdlar.Size = new System.Drawing.Size(227, 340);
+            this.lbAdlar.TabIndex = 0;
+            // 
+            // btnAra
+            // 
+            this.btnAra.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnAra.Location = new System.Drawing.Point(164, 35);
+            this.btnAra.Name = "btnAra";
+            this.btnAra.Size = new System.Drawing.Size(75, 31);
+            this.btnAra.TabIndex = 1;
+            this.btnAra.Text = "ARA";
+            this.btnAra.UseVisualStyleBackColor = true;
+            this.btnAra.Click += new System.EventHandler(this.btnAra_Click);
+            // 
+            // txtAranan
+            // 
+            this.txtAranan.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtAranan.Location = new System.Drawing.Point(12, 35);
+            this.txtAranan.Multiline = true;
+            this.txtAranan.Name = "txtAranan";
+            this.txtAranan.Size = new System.Drawing.Size(146, 31);
+            this.txtAranan.TabIndex = 2;
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSonuc.Location = new System.Drawing.Point(12, 465);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(25, 16);
+            this.lblSonuc.TabIndex = 3;
+            this.lblSonuc.Text = "***";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(267, 543);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.txtAranan);
+            this.Controls.Add(this.btnAra);
+            this.Controls.Add(this.lbAdlar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbAdlar;
+        private System.Windows.Forms.Button btnAra;
+        private System.Windows.Forms.TextBox txtAranan;
+        private System.Windows.Forms.Label lblSonuc;
+    }
+}
+
---
+++ rehber -96/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace rehber__96
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAra_Click(object sender, EventArgs e)
+        {
+            string aranan = txtAranan.Text;
+            int sayac = 0;
+            for (int i = 0; i < lbAdlar.Items.Count; i++)
+            {
+                if (aranan == lbAdlar.Items[i].ToString())
+                {
+                    sayac++;
+                }
+            }
+            if (sayac == 0)
+            {
+                lblSonuc.Text= "Kişi Bulunamadı";
+            }
+            else
+            {
+                lblSonuc.Text =sayac + "  Kişi Bulundu";
+            }
+        }
+    }
+}
